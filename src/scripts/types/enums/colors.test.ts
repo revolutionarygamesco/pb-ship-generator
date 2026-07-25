466e22e14210496a786f1c91d3b2138fdeb76827
Jason Godesky <jefgodesky@gmail.com>
2026-07-25 13:08:01 -0400

    Import Colors from Pirate Names

---
+++ src/scripts/types/enums/colors.test.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect } from 'vitest'
+import { primitives } from '@revolutionarygamesco/common/testing'
+import { mockTables } from '@revolutionarygamesco/common-foundryvtt/mocks'
+import getRollTableUUID from '../../utilities/get-rolltable-uuid.ts'
+import {
+  isColors,
+  selectRandomColors,
+  colors,
+  type Colors
+} from './colors.ts'
+
+describe('isColors', () => {
+  it.each(primitives)('rejects %s', (_desc: string, candidate: any) => {
+    expect(isColors(candidate)).toBe(false)
+  })
+
+  it.each(colors)('accepts %s', (colors: Colors) => {
+    expect(isColors(colors)).toBe(true)
+  })
+})
+
+describe('colors', () => {
+  it('picks colors', async () => {
+    mockTables({ [getRollTableUUID('CrljZ2S8EdjWco9K', 'NAMES')]: { results: [{ description: 'Spanish' } as foundry.documents.TableResult] } })
+    expect(await selectRandomColors()).toBe('Spanish')
+  })
+})
---
+++ src/scripts/types/enums/colors.ts
@@ -0,0 +1,12 @@
+import { makeEnum } from '@revolutionarygamesco/common'
+import { drawGuarded } from '@revolutionarygamesco/common-foundryvtt'
+import getRollTableUUID from '../../utilities/get-rolltable-uuid.ts'
+
+export const colors = ['Spanish', 'British', 'French', 'Dutch', 'Pirate'] as const
+export type Colors = typeof colors[number]
+export const { guard: isColors } = makeEnum(colors)
+
+export const selectRandomColors = async (): Promise<Colors> => {
+  const uuid = getRollTableUUID('CrljZ2S8EdjWco9K', 'NAMES')
+  return await drawGuarded(uuid, isColors, 'Spanish')
+}
