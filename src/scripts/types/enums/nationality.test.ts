b13ab0f25fb26db88a804bb606f99e5226b8a77f
Jason Godesky <jefgodesky@gmail.com>
2026-07-25 13:09:15 -0400

    Import Nationality from Pirate Names

---
+++ src/scripts/types/enums/nationality.test.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest'
+import { primitives } from '@revolutionarygamesco/common/testing'
+import { mockTables } from '@revolutionarygamesco/common-foundryvtt/mocks'
+import getRollTableUUID from '../../utilities/get-rolltable-uuid.ts'
+import {
+  isNationality,
+  selectRandomNationality,
+  nationalities,
+  type Nationality
+} from './nationality.ts'
+
+
+describe('isNationality', () => {
+  it.each(primitives)('rejects %s', (_desc: string, candidate: any) => {
+    expect(isNationality(candidate)).toBe(false)
+  })
+
+  it.each(nationalities)('accepts %s', (nationality: Nationality) => {
+    expect(isNationality(nationality)).toBe(true)
+  })
+})
+
+describe('selectRandomNationality', () => {
+  it('picks a nationality', async () => {
+    mockTables({ [getRollTableUUID('NLyKzSrJYnYaU6TJ', 'NAMES')]: { results: [{ description: 'Spanish' } as foundry.documents.TableResult] } })
+    expect(await selectRandomNationality()).toBe('Spanish')
+  })
+})
---
+++ src/scripts/types/enums/nationality.ts
@@ -0,0 +1,16 @@
+import { makeEnum } from '@revolutionarygamesco/common'
+import { drawGuarded } from '@revolutionarygamesco/common-foundryvtt'
+import getRollTableUUID from '../../utilities/get-rolltable-uuid.ts'
+
+export const nationalities = ['Akan', 'Bantu', 'Dutch',
+  'English', 'Fon', 'French', 'Igbo', 'Irish', 'Kalinago', 'Mandinka',
+  'Miskito', 'Portuguese', 'Scottish', 'Spanish', 'Taíno', 'Welsh', 'Yoruba'] as const
+export type Nationality = typeof nationalities[number]
+export const { guard: isNationality } = makeEnum(nationalities)
+
+export const selectRandomNationality = async (
+  scope: 'person' | 'pirate' = 'person'
+): Promise<Nationality> => {
+  const id = scope === 'pirate' ? 'S3jEhiwdL6Pry0nK' : 'NLyKzSrJYnYaU6TJ'
+  return await drawGuarded(getRollTableUUID(id, 'NAMES'), isNationality, 'Spanish')
+}
