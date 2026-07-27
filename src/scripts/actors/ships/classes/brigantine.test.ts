1e850a3986155c9c1ac24f5c24287e22d5b80f9c
Jason Godesky <jefgodesky@gmail.com>
2026-07-26 21:09:05 -0400

    Add new method: createBrigantine

---
+++ src/scripts/actors/ships/classes/brigantine.test.ts
@@ -0,0 +1,73 @@
+import { beforeEach, describe, it, expect } from 'vitest'
+import { isWithinRange } from '@revolutionarygamesco/common'
+import createBrigantine from './brigantine.ts'
+
+describe('createBrigantine', () => {
+  let actor: Partial<foundry.documents.Actor>
+
+  beforeEach(() => {
+    actor = createBrigantine()
+  })
+
+  it('sets the brigantine’s HP to 40', () => {
+    expect(actor.system?.attributes?.hp?.max).toBe(40)
+    expect(actor.system?.attributes?.hp?.value).toBe(40)
+  })
+
+  it('sets the brigantine’s hull to -d4', () => {
+    expect(actor.system?.attributes?.hull?.max).toBe(2)
+    expect(actor.system?.attributes?.hull?.value).toBe(2)
+    expect(actor.system?.attributes?.hull?.min).toBe(0)
+  })
+
+  it('sets the brigantine’s agility to +1', () => {
+    expect(actor.system?.abilities?.agility?.value).toBe(1)
+  })
+
+  it('sets the brigantine’s speed to 4', () => {
+    expect(actor.system?.attributes?.speed?.max).toBe(4)
+    expect(actor.system?.attributes?.speed?.value).toBe(4)
+    expect(actor.system?.attributes?.speed?.min).toBe(0)
+  })
+
+  it('sets the brigantine’s crew skill to 0', () => {
+    expect(actor.system?.abilities?.skill?.value).toBe(0)
+  })
+
+  it('sets the brigantine’s broadsides to d8', () => {
+    expect(actor.system?.weapons?.broadsides?.die).toBe('d8')
+    expect(actor.system?.weapons?.broadsides?.quantity).toBe(1)
+    expect(actor.system?.weapons?.broadsides?.warning).not.toBeDefined()
+  })
+
+  it('sets the brigantine’s small arms to d4', () => {
+    expect(actor.system?.weapons?.smallArms?.die).toBe('d4')
+    expect(actor.system?.weapons?.smallArms?.quantity).toBe(1)
+    expect(actor.system?.weapons?.smallArms?.warning).not.toBeDefined()
+  })
+
+  it('sets the brigantine’s ram to d6', () => {
+    expect(actor.system?.weapons?.ram?.die).toBe('d6')
+    expect(actor.system?.weapons?.ram?.quantity).toBe(1)
+    expect(actor.system?.weapons?.ram?.warning).not.toBeDefined()
+  })
+
+  it('sets the fluyt’s cargo capacity to 3', () => {
+    expect(actor.system?.attributes?.cargo?.max).toBe(3)
+    expect(actor.system?.attributes?.cargo?.value).toBe(0)
+  })
+
+  it('sets the brigantine’s crew to 15/30', () => {
+    expect(actor.system?.attributes?.crew?.min).toBe(15)
+    expect(actor.system?.attributes?.crew?.max).toBe(30)
+    expect(isWithinRange(actor.system?.attributes?.crew?.value ?? 0, [15, 30])).toBe(true)
+  })
+
+  it('sets description', () => {
+    expect(actor.system?.description).toBe('revolutionary-pbshipgen.ships.brigantine.description')
+  })
+
+  it('sets special', () => {
+    expect(actor.system?.special).not.toBeDefined()
+  })
+})
---
+++ src/scripts/actors/ships/classes/brigantine.ts
@@ -0,0 +1,34 @@
+import { scopeLocalizer } from '@revolutionarygamesco/common-foundryvtt'
+import { MODULE_ID } from '../../../settings.ts'
+
+import setShipHP from '../hp.ts'
+import setHull from '../hull.ts'
+import setShipAgility from '../agility.ts'
+import setShipSpeed from '../speed.ts'
+import setShipCrewSkill from '../crew-skill.ts'
+import setBroadsides from '../broadsides.ts'
+import setSmallArms from '../small-arms.ts'
+import setRamming from '../ram.ts'
+import setCrewSize from '../crew-size.ts'
+import setCargoCapacity from '../cargo.ts'
+
+const createBrigantine = (): Partial<foundry.documents.Actor> => {
+  const actor: Partial<foundry.documents.Actor> = {}
+
+  setShipHP(actor, 40)
+  setHull(actor, '-d4')
+  setShipAgility(actor, 1)
+  setShipSpeed(actor, 4)
+  setShipCrewSkill(actor, 0)
+  setBroadsides(actor, 'd8', 1)
+  setSmallArms(actor, 'd4', 1)
+  setRamming(actor, 'd6', 1)
+  setCrewSize(actor, 15 ,30)
+  setCargoCapacity(actor, 3)
+
+  const t = scopeLocalizer([MODULE_ID, 'ships', 'brigantine'].join('.'))
+  actor.system!.description = t('description')
+  return actor
+}
+
+export default createBrigantine
