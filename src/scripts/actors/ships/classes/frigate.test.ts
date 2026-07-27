6233c2deffeb80ed3b985e35be9cd1207305a98e
Jason Godesky <jefgodesky@gmail.com>
2026-07-26 21:16:45 -0400

    Add new method: createFrigate

---
+++ src/scripts/actors/ships/classes/frigate.test.ts
@@ -0,0 +1,73 @@
+import { beforeEach, describe, it, expect } from 'vitest'
+import { isWithinRange } from '@revolutionarygamesco/common'
+import createFrigate from './frigate.ts'
+
+describe('createFrigate', () => {
+  let actor: Partial<foundry.documents.Actor>
+
+  beforeEach(() => {
+    actor = createFrigate()
+  })
+
+  it('sets the frigate’s HP to 50', () => {
+    expect(actor.system?.attributes?.hp?.max).toBe(60)
+    expect(actor.system?.attributes?.hp?.value).toBe(60)
+  })
+
+  it('sets the frigate’s hull to -d4', () => {
+    expect(actor.system?.attributes?.hull?.max).toBe(2)
+    expect(actor.system?.attributes?.hull?.value).toBe(2)
+    expect(actor.system?.attributes?.hull?.min).toBe(0)
+  })
+
+  it('sets the frigate’s agility to 0', () => {
+    expect(actor.system?.abilities?.agility?.value).toBe(0)
+  })
+
+  it('sets the frigate’s speed to 4', () => {
+    expect(actor.system?.attributes?.speed?.max).toBe(4)
+    expect(actor.system?.attributes?.speed?.value).toBe(4)
+    expect(actor.system?.attributes?.speed?.min).toBe(0)
+  })
+
+  it('sets the frigate’s crew skill to 1', () => {
+    expect(actor.system?.abilities?.skill?.value).toBe(1)
+  })
+
+  it('sets the frigate’s broadsides to 2d8', () => {
+    expect(actor.system?.weapons?.broadsides?.die).toBe('d8')
+    expect(actor.system?.weapons?.broadsides?.quantity).toBe(2)
+    expect(actor.system?.weapons?.broadsides?.warning).toBe('revolutionary-pbshipgen.ships.frigate.broadsides')
+  })
+
+  it('sets the frigate’s small arms to d6', () => {
+    expect(actor.system?.weapons?.smallArms?.die).toBe('d6')
+    expect(actor.system?.weapons?.smallArms?.quantity).toBe(1)
+    expect(actor.system?.weapons?.smallArms?.warning).not.toBeDefined()
+  })
+
+  it('sets the frigate’s ram to d6', () => {
+    expect(actor.system?.weapons?.ram?.die).toBe('d6')
+    expect(actor.system?.weapons?.ram?.quantity).toBe(1)
+    expect(actor.system?.weapons?.ram?.warning).not.toBeDefined()
+  })
+
+  it('sets the frigate’s cargo capacity to 4', () => {
+    expect(actor.system?.attributes?.cargo?.max).toBe(4)
+    expect(actor.system?.attributes?.cargo?.value).toBe(0)
+  })
+
+  it('sets the frigate’s crew to 24/48', () => {
+    expect(actor.system?.attributes?.crew?.min).toBe(24)
+    expect(actor.system?.attributes?.crew?.max).toBe(48)
+    expect(isWithinRange(actor.system?.attributes?.crew?.value ?? 0, [24, 48])).toBe(true)
+  })
+
+  it('sets description', () => {
+    expect(actor.system?.description).toBe('revolutionary-pbshipgen.ships.frigate.description')
+  })
+
+  it('sets special', () => {
+    expect(actor.system?.special).not.toBeDefined()
+  })
+})
---
+++ src/scripts/actors/ships/classes/frigate.ts
@@ -0,0 +1,34 @@
+import { scopeLocalizer } from '@revolutionarygamesco/common-foundryvtt'
+import { MODULE_ID } from '../../../settings.ts'
+
+import setShipHP from '../hp.ts'
+import setHull from '../hull.ts'
+import setShipAgility from '../agility.ts'
+import setShipSpeed from '../speed.ts'
+import setShipCrewSkill from '../crew-skill.ts'
+import setBroadsides from '../broadsides.ts'
+import setSmallArms from '../small-arms.ts'
+import setRamming from '../ram.ts'
+import setCrewSize from '../crew-size.ts'
+import setCargoCapacity from '../cargo.ts'
+
+const createFrigate = (): Partial<foundry.documents.Actor> => {
+  const actor: Partial<foundry.documents.Actor> = {}
+  const t = scopeLocalizer([MODULE_ID, 'ships', 'frigate'].join('.'))
+
+  setShipHP(actor, 60)
+  setHull(actor, '-d4')
+  setShipAgility(actor, 0)
+  setShipSpeed(actor, 4)
+  setShipCrewSkill(actor, 1)
+  setBroadsides(actor, 'd8', 2, t('broadsides'))
+  setSmallArms(actor, 'd6', 1)
+  setRamming(actor, 'd6', 1)
+  setCrewSize(actor, 24 ,48)
+  setCargoCapacity(actor, 4)
+
+  actor.system!.description = t('description')
+  return actor
+}
+
+export default createFrigate
