4b5290dbfdb194f3e15098ef78b425f1b03cd5d0
Jason Godesky <jefgodesky@gmail.com>
2026-07-27 16:52:19 -0400

    nameShip: Retry until open folder space is found

--- src/scripts/names/ship.ts
+++ src/scripts/names/ship.ts
@@ -1,5 +1,8 @@
+import { retryUntil } from '@revolutionarygamesco/common'
 import { type Colors } from '../types/enums/colors.ts'
 import { type ShipRole } from '../types/enums/role.ts'
+import findCategoryFolder from '../utilities/find-folder.ts'
+import getShipActorName from './selectors/ship-actor.ts'
 
 const nameShip = async (
   colors: Colors,
@@ -8,7 +11,19 @@ const nameShip = async (
 ): Promise<Record<string, string>> => {
   const module = game.modules.get('revolutionary-piratenames')
   if (!module) throw new Error('Could not load Pirate Names module.')
-  return await module.api.generateShipName({ colors, role, privateer })
+
+  const category = findCategoryFolder(colors, role, privateer)
+  if (!category) throw new Error('Could not load ship category folder.')
+
+  const names = await retryUntil(async (): Promise<Record<string, string>> => {
+    return module.api.generateShipName({ colors, role, privateer })
+  }, (names: Record<string, string>) => {
+    const name = getShipActorName(colors, names)
+    return category.children.every(child => child.folder.name !== name)
+  }, { fallback: {} })
+
+  if (Object.keys(names).length < 1) throw new Error('Could not find an available name.')
+  return names
 }
 
 export default nameShip
