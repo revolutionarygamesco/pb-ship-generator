643ad91bfe897990bb8f19314d79b3b50e2d0ef0
Jason Godesky <jefgodesky@gmail.com>
2026-07-29 18:31:18 -0400

    randomizeMagician: Separate priest, sorcerer

--- src/scripts/randomizers/crew/magician.test.ts
+++ src/scripts/randomizers/crew/magician.test.ts
@@ -3,9 +3,15 @@ import getFeatureUUID from '../../utilities/get-feature-uuid.ts'
 import randomizeDeckMagician from './magician.ts'
 
 describe('randomizeDeckMagician', () => {
-  it('might return a deck sorcerer or a deck priest', () => {
-    const valid = [null, getFeatureUUID('oh2VItoo8q4i49GX'), getFeatureUUID('0KDL3PjlyVOFQUxG')]
-    const actual = randomizeDeckMagician()
-    expect(valid).toContain(actual)
+  it('might return a deck sorcerer', () => {
+    const valid = [null, getFeatureUUID('0KDL3PjlyVOFQUxG')]
+    const { sorcerer } = randomizeDeckMagician()
+    expect(valid).toContain(sorcerer)
+  })
+
+  it('might return a deck priest', () => {
+    const valid = [null, getFeatureUUID('oh2VItoo8q4i49GX')]
+    const { priest } = randomizeDeckMagician()
+    expect(valid).toContain(priest)
   })
 })
--- src/scripts/randomizers/crew/magician.ts
+++ src/scripts/randomizers/crew/magician.ts
@@ -1,11 +1,17 @@
 import { selectRandomBetween } from '@revolutionarygamesco/common'
 import getFeatureUUID  from '../../utilities/get-feature-uuid.ts'
 
-const randomizeMagician = (): string | null => {
+interface MagicianCrew {
+  priest: string | null
+  sorcerer: string | null
+}
+
+const randomizeMagician = (): MagicianCrew => {
+  const data: MagicianCrew = { priest: null, sorcerer: null }
   const r = selectRandomBetween(1, 20)
-  if (r === 1) return getFeatureUUID('0KDL3PjlyVOFQUxG')
-  if (r === 20) return getFeatureUUID('oh2VItoo8q4i49GX')
-  return null
+  if (r === 1) data.sorcerer = getFeatureUUID('0KDL3PjlyVOFQUxG')
+  if (r === 20) data.priest = getFeatureUUID('oh2VItoo8q4i49GX')
+  return data
 }
 
 export default randomizeMagician
