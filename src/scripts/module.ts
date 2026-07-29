a53b9742b1a568776a2bd22c62f8c7bef43e8300
Jason Godesky <jefgodesky@gmail.com>
2026-07-29 18:48:45 -0400

    Move generateShip into main directory

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -2,7 +2,7 @@ import '@revolutionarygamesco/common-foundryvtt/systems/pirateborg'
 import { scopeLocalizer, registerAPI } from '@revolutionarygamesco/common-foundryvtt'
 import { MODULE_ID } from './settings'
 
-import generateShip from './generators/ship.ts'
+import generateShip from './ship.ts'
 
 registerAPI(MODULE_ID, {
   generateShip
