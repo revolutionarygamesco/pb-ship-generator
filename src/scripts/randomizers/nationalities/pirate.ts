4fe61933394e1e6fdd7bc137f3a9b1b31a044270
Jason Godesky <jefgodesky@gmail.com>
2026-07-30 21:16:02 -0400

    Fix pirate nationality proportions

--- src/scripts/randomizers/nationalities/pirate.ts
+++ src/scripts/randomizers/nationalities/pirate.ts
@@ -4,19 +4,19 @@ import { type Nationality } from '../../types/enums/nationality.ts'
 const selectRandomPirateNationality = (): Nationality => {
   return selectRandomElement(stockArray<Nationality>([
     { n: 33, item: 'English' },
-    { n: 30, item: 'Akan' },
-    { n: 20, item: 'Yoruba' },
-    { n: 20, item: 'Fon' },
-    { n: 15, item: 'Bantu' },
-    { n: 10, item: 'Mandinka' },
     { n: 10, item: 'Scottish' },
     { n: 8, item: 'Welsh' },
-    { n: 5, item: 'Dutch' },
-    { n: 5, item: 'Igbo' },
+    { n: 6, item: 'Akan' },
     { n: 5, item: 'Irish' },
+    { n: 5, item: 'Dutch' },
     { n: 4, item: 'French' },
+    { n: 4, item: 'Yoruba' },
+    { n: 4, item: 'Fon' },
+    { n: 3, item: 'Bantu' },
+    { n: 2, item: 'Mandinka' },
     { n: 2, item: 'Spanish' },
     { n: 2, item: 'Miskito' },
+    { n: 1, item: 'Igbo' },
     { n: 1, item: 'Portuguese' }
   ]))
 }
